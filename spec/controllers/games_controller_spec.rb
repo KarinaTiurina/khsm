42b929524b4e0dfd81b3345994db9b1bfb1cb87f
KarinaTiurina <rktiurina@gmail.com>
2017-09-15 21:06:33 +0300

    rspec: GamesController #take_money

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -73,5 +73,25 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(game_path(game))
       expect(flash.empty?).to be_truthy
     end
+
+    context 'When #take_money before finished' do
+      it 'finish game with prize' do
+        game_w_questions.update_attribute(:current_level, 2)
+
+        put :take_money,
+            id: game_w_questions.id
+
+        game = assigns(:game)
+
+        expect(game.finished?).to be_truthy
+        expect(game.prize).to eq 200
+
+        expect(response).to redirect_to(user_path(user))
+        expect(flash[:warning]).to be
+
+        user.reload
+        expect(user.balance).to eq 200
+      end
+    end
   end
 end
